a118d0c093d0dde4df1326057c69df2c5d20e3a1
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-04-15 16:53:46 +0530

    for testing

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -3,3 +3,4 @@ const mongoose = require("mongoose")
 module.exports = () => {
     return mongoose.connect(`mongodb+srv://AnkitMishra07:ankit07@cluster0.jxwud.mongodb.net/meesho_clone`)
 }
+let test = ""
